9b8e47dd381fe38f0ed79f8b49c2d8be7f1bb144
mehrelibehruz <mehrelibehruz@gmail.com>
2023-11-19 14:38:53 +0400

    Add to TMP Pro and Create "NewFact" button/

--- Assets/Script/JSonText.cs
+++ Assets/Script/JSonText.cs
@@ -1,6 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class JSonText : MonoBehaviour
 {
+    IEnumerator StartJson()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get("https://catfact.ninja/fact"))
+        {
+            yield return request.SendWebRequest();
 
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                JObject o = JObject.Parse(request.downloadHandler.text);
+                Debug.Log(o["fact"].ToString());
+            }
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(StartJson());
+    }
+    public void NewFact()
+    {
+        StartCoroutine(StartJson());
+    }
 }
